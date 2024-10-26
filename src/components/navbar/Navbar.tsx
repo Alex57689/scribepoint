0fd61d8441b5e07a6d00e91f00c13f69ef73a43e
Alex Costello <Alexcostello20@gmail.com>
2024-10-26 23:04:33 +1000

    create homepage

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import {
+  Typography,
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Drawer,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Navbar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Photos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      {/* Drawer Sidebar */}
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={isDrawerOpen}
+        onClose={toggleDrawer}
+        sx={{
+          "& .MuiDrawer-paper": {
+            backgroundColor: "secondary.main", // Custom background color here
+          },
+        }}
+      >
+        <Box
+          sx={{ width: 250 }}
+          onClick={toggleDrawer}
+          onKeyDown={toggleDrawer}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginRight: 5,
+              marginLeft: 1,
+            }}
+          >
+            <Typography variant="h6" sx={{ p: 2 }}>
+              Projects
+            </Typography>
+            <IconButton>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+          {/* Add sidebar items here */}
+        </Box>
+      </Drawer>
+    </>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { Typography, Box, Container } from "@mui/material";
+import { Box } from "@mui/material";
+import Navbar from "../components/navbar/Navbar";
 
 const Home = () => {
   return (
@@ -7,24 +8,9 @@ const Home = () => {
         sx={{
           backgroundColor: "#1A1A1A",
           minHeight: "100vh", // Full height for the page
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
         }}
       >
-        <Container
-          maxWidth="sm"
-          sx={{
-            padding: 3,
-            borderRadius: 2,
-            position: "absolute",
-            top: 0,
-          }}
-        >
-          <Typography variant="h1" fontWeight={500} color="primary">
-            Scribe Point
-          </Typography>
-        </Container>
+        <Navbar />
       </Box>
     </>
   );
