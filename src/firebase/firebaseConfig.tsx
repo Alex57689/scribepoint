9ee70768cadd75af51b819a244ab0d2398471d18
Alex Costello <Alexcostello20@gmail.com>
2024-11-17 22:15:11 +1000

    fixed firebaseConfig file

--- src/firebase/firebaseConfig.tsx
+++ src/firebase/firebaseConfig.tsx
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 //firebase confirm
-/*
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTHDOMAIN,
@@ -11,8 +11,9 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_APP_FIREBASE_APPID,
   measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENTID,
-};*/
+};
 
+/*
 const firebaseConfig = {
   apiKey: "AIzaSyD6J9HNVleqw-qf_NPe3ZAX6EavcE8iJAc",
   authDomain: "scribepoint-e575f.firebaseapp.com",
@@ -22,7 +23,7 @@ const firebaseConfig = {
   appId: "1:66443196930:web:9d57454e8b03c8fb6c451b",
   measurementId: "G-0W4SBPQR50",
 };
-
+*/
 // initalise firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
